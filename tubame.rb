8622129418b231c3aa7891b46cbb1cce8a318b7f
Eiji Takahashi <tak7iji@gmail.com>
2014-07-16 18:58:36 +0900

    Update tubame.rb

--- tubame.rb
+++ tubame.rb
@@ -7,35 +7,39 @@ $base_name = File.split(Dir.getwd)[1]
 C=:c
 N=:n
 
+def const_file cond, file
+  cond ? "" : File.join($base_name, file)
+end
+
 def search_regex type, key1, key2
   r1 = Regexp.new(key2.empty? ? "[.]*" : key1)
   r2 = Regexp.new(key2.empty? ? key1 : key2)
 
   h = lambda {|s| s.is_a?(Array)}
+  i = lambda {|r| r.any? && r.last == C}
   f = lambda do |file, r, e|
-    (N if (e[0].start_with?("//")) || (r.any? && r.last == C && e[0].end_with?("*/")))||
-    (C if (e[0].start_with?("/*") && !e[0].end_with?("*/")) || (r.any? && r.last == C))||
-    ([r.any?(&h) ? "" : File.join($base_name, file), e[1] + 1, e[0].gsub("\"", "\"\"")] if e[0].match(r2))
+    (N if (e[0].start_with?("//")) || (i[r] && e[0].end_with?("*/")))||
+    (C if (e[0].start_with?("/*") && !e[0].end_with?("*/")) || i[r])||
+    ([const_file(r.any?(&h), file), e[1] + 1, e[0].gsub('"', '""')] if e[0].match(r2))
   end.curry
 
   Dir.glob("**/#{type}").lazy.map do |file|
     g = f[file]
-    lines = File.readlines(file).map(&:strip)
+    lines = open(file).lazy.map(&:strip).to_a
     next if lines.none? {|e| e.match(r1)}
 
-    lines.each_with_index.inject([]) do |r, e|
+    lines.lazy.each_with_index.inject([]) do |r, e|
       r + [g[r, e]]
-    end.compact.select(&h)
+    end.compact.select(&h).to_a
   end.to_a.compact.flatten 1
 end
 
 def search_xpath type, key1, key2
   Dir.glob("**/#{type}").lazy.map do |file|
     body = open(file, "rt", &:read)
-    Nokogiri.XML(body).xpath(key1).map.with_index do |node, idx|
-      line = body.split("\n")[node.line - 1].strip
-      [idx == 0 ? File.join($base_name, file) : "", node.line, line]
-    end
+    Nokogiri.XML(body).xpath(key1).lazy.with_index.map do |node, idx|
+      [const_file(idx != 0, file), node.line, body.split("\n")[node.line - 1].strip]
+    end.to_a
   end.to_a.flatten 1
 end
 
@@ -50,16 +54,15 @@ xml.remove_namespaces!.xpath('//ChapterCategoryRefKey').each do |e|
   next if kh_key.empty?
 
   xml.xpath("//KnowhowInfomation[@knowhowId='#{kh_key}']/CheckItem").each do |check_item|
-    search_key = check_item.attr('searchRefKey')
-    next if search_key.nil? || search_key.empty?
+    s_key = check_item.attr('searchRefKey')
+    next if s_key.nil? || s_key.empty?
 
     process = check_item.xpath('SearchProcess').text
-    s_key = search_key
-    xml.xpath("//SearchInfomation[@searchInfoId='#{search_key}']").each do |search_info|
-      args = %w(FileType SearchKey1 SearchKey2).map{|e| search_info.xpath(e)[0].text}
+    xml.xpath("//SearchInfomation[@searchInfoId='#{s_key}']").each do |s_info|
+      args = %w(FileType SearchKey1 SearchKey2).map{|e| s_info.xpath(e)[0].text}
 
-      (search_info.xpath('PythonModule')[0].text.empty? ? search_regex(*args) : search_xpath(*args)).each_with_index do |line, idx|
-        csv << (idx == 0 ? [chap_no, process, s_key] : [""]*3)+line
+      (s_info.xpath('PythonModule')[0].text.empty? ? search_regex(*args) : search_xpath(*args)).each_with_index do |l, i|
+        csv << (i == 0 ? [chap_no, process, s_key] : [""]*3) + l
       end
     end
   end
